6ff4229080fbb04f86566ebab6de944b69d220fa
Farhan Rozaidi <farhanrozaidi@10-249-181-4.wireless.oregonstate.edu>
2025-06-24 09:27:34 -0700

    Added 1, 2, and 3 cam experiments

--- read.py
+++ read.py
@@ -7,11 +7,12 @@ import time
 import threading
 
 # Serial port configuration (CHANGE THIS to match your setup)
-SERIAL_PORT = '/dev/tty.usbmodem1303'
+SERIAL_PORT = '/dev/tty.usbmodem1403'
 BAUD_RATE = 1000000
 
 # Serial port open
 ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
+ser.write("Reset\n".encode('ascii'))
 
 # Plot buffer
 max_points = 200
@@ -32,8 +33,13 @@ buffer_lock = threading.Lock()
 
 # Serial reading thread
 def read_serial():
+    # starttime = time.time()
     while True:
         try:
+            # cur_time = (time.time()-starttime)
+            # if cur_time >5.0:
+            #     ser.write("Reset".encode('ascii'))
+            #     print(cur_time)
             line = ser.readline().decode('utf-8').strip()
             if line:
                 parts = line.split(',')
